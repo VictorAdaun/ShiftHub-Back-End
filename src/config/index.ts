6cd687e14e16e0dab7a4aa17f4854d492d3fba4b
bisishitta <shitta19@gmail.com>
2023-11-04 18:39:22 +0100

    unnecessary files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,28 +1,28 @@
-import * as dotenv from 'dotenv'
-import path from 'path'
+import * as dotenv from "dotenv";
+import path from "path";
 
-import { config } from './config'
+import { config } from "./config";
 // load .env and .secret files into process.env
-dotenv.config()
+dotenv.config();
 dotenv.config({
-  path: path.resolve(process.cwd(), './.secret'),
-})
+  path: path.resolve(process.cwd(), "./.secret"),
+});
 
 export default config.load({
-  ENV: (process.env.NODE_ENV as string) || 'development',
-  IS_DEV: process.env.NODE_ENV !== 'production',
-  IS_TEST: process.env.NODE_ENV === 'test',
-  IS_PROD: process.env.NODE_ENV === 'production',
-  JWT_SECRET: (process.env.JWT_SECRET as string) || '',
-  LOG_LEVEL: (process.env.LOG_LEVEL as string) || 'info',
-  GOOGLE_CLIENT_ID: (process.env.GOOGLE_CLIENT_ID as string) || '',
-  GOOGLE_CLIENT_SECRET: (process.env.GOOGLE_CLIENT_SECRET as string) || '',
-  CLOUD_NAME: (process.env.CLOUD_NAME as string) || '',
-  CLOUD_API_KEY: (process.env.CLOUD_API_KEY as string) || '',
-  CLOUD_API_SECRET: (process.env.CLOUD_API_SECRET as string) || '',
-  NODEMAILER_USER: (process.env.NODEMAILER_USER as string) || '',
-  NODEMAILER_PASS: (process.env.NODEMAILER_PASS as string) || '',
-  NODEMAILER_ACCOUNT: (process.env.NODEMAILER_ACCOUNT as string) || '',
-  REFRESH_TOKEN: (process.env.REFRESH_TOKEN as string) || '',
-  REDIRECT_URI: (process.env.REDIRECT_URi as string) || '',
-})
+  ENV: (process.env.NODE_ENV as string) || "development",
+  IS_DEV: process.env.NODE_ENV !== "production",
+  IS_TEST: process.env.NODE_ENV === "test",
+  IS_PROD: process.env.NODE_ENV === "production",
+  JWT_SECRET: (process.env.JWT_SECRET as string) || "",
+  LOG_LEVEL: (process.env.LOG_LEVEL as string) || "info",
+  GOOGLE_CLIENT_ID: (process.env.GOOGLE_CLIENT_ID as string) || "",
+  GOOGLE_CLIENT_SECRET: (process.env.GOOGLE_CLIENT_SECRET as string) || "",
+  CLOUD_NAME: (process.env.CLOUD_NAME as string) || "",
+  CLOUD_API_KEY: (process.env.CLOUD_API_KEY as string) || "",
+  CLOUD_API_SECRET: (process.env.CLOUD_API_SECRET as string) || "",
+  NODEMAILER_USER: (process.env.NODEMAILER_USER as string) || "",
+  NODEMAILER_PASS: (process.env.NODEMAILER_PASS as string) || "",
+  NODEMAILER_ACCOUNT: (process.env.NODEMAILER_ACCOUNT as string) || "",
+  REFRESH_TOKEN: (process.env.REFRESH_TOKEN as string) || "",
+  REDIRECT_URI: (process.env.REDIRECT_URi as string) || "",
+});
--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,88 +1,96 @@
-import * as pug from 'pug'
-import nodemailer from 'nodemailer'
-import config from '../config'
-import { google } from 'googleapis'
-import { Options } from 'nodemailer/lib/smtp-transport'
-import { resolve } from 'path'
+import * as pug from "pug";
+import nodemailer from "nodemailer";
+import config from "../config";
 export async function sendEmail(mailContext: any) {
   const html = pug.renderFile(
-    process.cwd() + '/src/utils/' + mailContext.type + '.pug',
+    process.cwd() + "/src/utils/" + mailContext.type + ".pug",
     mailContext.data
-  )
+  );
 
-  const subject = 'Welcome to EZScheduler'
-  complete(html, mailContext.data.email, subject)
+  const subject = "Welcome to EZScheduler";
+  complete(html, mailContext.data.email, subject);
 }
 
 export async function sendWelcomeEmail(mailContext: any) {
   const html = pug.renderFile(
-    process.cwd() + '/src/utils/welcome.pug',
+    process.cwd() + "/src/utils/welcome.pug",
     mailContext.data
-  )
+  );
 
-  const subject = 'Welcome to EZScheduler'
-  complete(html, mailContext.data.email, subject)
+  const subject = "Welcome to EZScheduler";
+  complete(html, mailContext.data.email, subject);
 }
 
 export async function changePasswordEmail(mailContext: any) {
   const html = pug.renderFile(
-    process.cwd() + '/src/utils/resetPassword.pug',
+    process.cwd() + "/src/utils/resetPassword.pug",
     mailContext.data
-  )
+  );
 
-  const subject = 'Reset password'
-  complete(html, mailContext.data.email, subject)
+  const subject = "Reset password";
+  complete(html, mailContext.data.email, subject);
 }
 
 export async function complete(html: any, email: string, subject: string) {
   try {
-    const OAuth2 = google.auth.OAuth2
-    const OAuth2Client = new OAuth2(
-      config('GOOGLE_CLIENT_ID'),
-      config('GOOGLE_CLIENT_SECRET'),
-      config('REDIRECT_URI')
-    )
-    console.log(config('REFRESH_TOKEN'))
-    OAuth2Client.setCredentials({ refresh_token: config('REFRESH_TOKEN') })
-    const ACCESS_TOKEN: string = await new Promise((resolve, reject) => {
-      OAuth2Client.getAccessToken((err, token) => {
-        if (err) {
-          console.log({ err })
-          reject('Failed to create access token')
-        }
-        resolve(token as string)
-      })
-    })
-    const configuration: Options = {
-      service: 'gmail',
+    // const OAuth2 = google.auth.OAuth2
+    // const OAuth2Client = new OAuth2(
+    //   config('GOOGLE_CLIENT_ID'),
+    //   config('GOOGLE_CLIENT_SECRET'),
+    //   config('REDIRECT_URI')
+    // )
+    // console.log(config('REFRESH_TOKEN'))
+    // OAuth2Client.setCredentials({ refresh_token: config('REFRESH_TOKEN') })
+    // const ACCESS_TOKEN: string = await new Promise((resolve, reject) => {
+    //   OAuth2Client.getAccessToken((err, token) => {
+    //     if (err) {
+    //       console.log({ err })
+    //       reject('Failed to create access token')
+    //     }
+    //     resolve(token as string)
+    //   })
+    // })
+    // const configuration: Options = {
+    //   service: 'gmail',
+    //   auth: {
+    //     type: 'OAuth2',
+    //     user: config('NODEMAILER_USER'),
+    //     clientId: config('GOOGLE_CLIENT_ID'),
+    //     clientSecret: config('GOOGLE_CLIENT_SECRET'),
+    //     refreshToken: config('REFRESH_TOKEN'),
+    //     accessToken: ACCESS_TOKEN,
+    //   },
+    //   port: 25,
+    //   secure: false,
+    //   tls: {
+    //     rejectUnauthorized: true,
+    //   },
+    // }
+    // const transporter = nodemailer.createTransport(configuration)
+
+    const transporter = nodemailer.createTransport({
+      host: config("NODEMAILER_ACCOUNT"),
+      port: 465,
+      secure: true,
       auth: {
-        type: 'OAuth2',
-        user: config('NODEMAILER_USER'),
-        clientId: config('GOOGLE_CLIENT_ID'),
-        clientSecret: config('GOOGLE_CLIENT_SECRET'),
-        refreshToken: config('REFRESH_TOKEN'),
-        accessToken: ACCESS_TOKEN,
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: config("NODEMAILER_USER"),
+        pass: config("NODEMAILER_PASS"),
       },
-      port: 25,
-      secure: false,
-      tls: {
-        rejectUnauthorized: true,
-      },
-    }
-    const transporter = nodemailer.createTransport(configuration)
+    });
     const mailOptions = {
-      from: config('NODEMAILER_ACCOUNT'),
+      from: config("NODEMAILER_ACCOUNT"),
       to: email,
       subject,
       html,
-    }
+    };
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        return console.log(error)
+        return console.log(error);
       }
-      console.log('Message sent: %s', info.messageId)
-    })
+      console.log("Message sent: %s", info.messageId);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
